2f1c368273acb1bad82ae54e30e10d9203a38670
FlutLab.io <>
2022-05-24 09:15:52 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -75,7 +75,103 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
 
   Widget build(BuildContext context, double shrinkOffset, bool overlabsContents) {
     return new Container(
-      child: _tabBar,
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 70,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.black, image: DecorationImage(image: AssetImage("assets/images/1.jpg"), fit: BoxFit.cover)),
+              ),
+              title: Text("Let me Love you", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14)),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("DJ Snake", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500, fontSize: 12)),
+                    Text("4.38", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12))
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 70,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.black, image: DecorationImage(image: AssetImage("assets/images/6.jpg"), fit: BoxFit.cover)),
+              ),
+              title: Text("Love me like you do", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14)),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("DJ Snake", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500, fontSize: 12)),
+                    Text("4.38", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12))
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 70,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.black, image: DecorationImage(image: AssetImage("assets/images/8.jpg"), fit: BoxFit.cover)),
+              ),
+              title: Text("Don't Let me down", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14)),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("DJ Snake", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500, fontSize: 12)),
+                    Text("4.38", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12))
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 70,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.black, image: DecorationImage(image: AssetImage("assets/images/9.jpg"), fit: BoxFit.cover)),
+              ),
+              title: Text("Take me with you", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14)),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("DJ Snake", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500, fontSize: 12)),
+                    Text("4.38", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12))
+                  ],
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Container(
+                width: 50,
+                height: 70,
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.black, image: DecorationImage(image: AssetImage("assets/images/7.jpg"), fit: BoxFit.cover)),
+              ),
+              title: Text("Feel Me", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 14)),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 7),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("DJ Snake", style: TextStyle(color: Colors.grey, fontWeight: FontWeight.w500, fontSize: 12)),
+                    Text("4.38", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 12))
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 
