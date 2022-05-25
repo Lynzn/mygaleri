123090c3abecd80b170e49bb1b782bfdc613a31a
FlutLab.io <>
2022-05-25 04:47:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,387 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class detailProduk extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BelajarAppBar(),
+      home: ListViewHome(),
     );
   }
 }
 
-class BelajarAppBar extends StatelessWidget {
-  @override
+class ListViewHome extends StatelessWidget {
+  var date = DateTime.now();
   Widget build(BuildContext context) {
     return Scaffold(
-      body: DefaultTabController(
-        length: 3,
-        child: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverAppBar(
-                expandedHeight: 200.0,
-                floating: false,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  centerTitle: true,
-                  background: Image(
-                    image: NetworkImage('https://images7.alphacoders.com/114/1144794.jpg'),
-                    fit: BoxFit.cover,
-                  ),
+        appBar: AppBar(
+          title: Text('Toko Online'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage('https://images.tokopedia.net/img/cache/700/product-1/2020/4/27/620102302/620102302_7c350566-6e96-4f96-a514-1187b0760e4b_1100_1100.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: _SliverAppBarDelegate(
-                  TabBar(
-                    labelColor: Colors.black87,
-                    unselectedLabelColor: Colors.grey,
-                    tabs: [
-                      new Tab(icon: new Icon(Icons.audiotrack), text: "Song"),
-                      new Tab(icon: new Icon(Icons.download), text: "Galery"),
-                      new Tab(icon: new Icon(Icons.favorite), text: "Playlist"),
-                    ],
-                  ),
-                ),
-              ),
-            ];
-          },
-          body: TabBarView(children: <Widget>[
-            Song(),
-            Galery(),
-            Playlist(),
-          ]),
-        ),
-      ),
-    );
-  }
-}
-
-class Song extends StatelessWidget {
-  final List song = [
-    "Sesuatu di Jogja",
-    "Pulang",
-    "Desember",
-    "Hujan di Mimpi",
-    "Senandung Maaf",
-    "Akad",
-    "Aku Tenang",
-    "Dialog Hujan",
-    "Sebelah Mata",
-    "Sampai Jadi Debu",
-    "Menuju Senja",
-    "Aroma Dia"
-  ];
-
-  final List singer = [
-    "Adhitia Sofyan",
-    "Float",
-    "Efek Rumah Kaca",
-    "Banda Neira",
-    "White Shoes",
-    "Payung Teduh",
-    "Fourtwnty",
-    "Senar Senja",
-    "Efek Rumah Kaca",
-    "Banda Neira",
-    "Payung Teduh",
-    "Sisitipsi"
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-              child: ListTile(
-            title: Text(song[index], style: TextStyle(fontSize: 20)),
-            subtitle: Text("Lagu dari " + singer[index]),
-            leading: Icon(Icons.audiotrack),
-          ));
-        },
-        itemCount: song.length,
-      ),
-    );
-  }
-}
-
-class Galery extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView.count(crossAxisCount: 2, children: <Widget>[
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i1.sndcdn.com/artworks-000259867817-48awxi-t500x500.jpg",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Sesuatu di Jogja - Adhitia Sofyan',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i1.sndcdn.com/artworks-000073226104-qwl397-t500x500.jpg",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Pulang - Float',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b273c72f1ef7daf938592186bc85",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Desember - Efek Rumah Kaca',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i1.sndcdn.com/artworks-000123190347-nsgu34-t500x500.jpg",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Hujan di Mimpi - Banda Neira',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b2737478de646383e04594b568a3",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Senandung Maaf - White Shoes',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b2737b2ee058353a3180f72ec03b",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Akad - Payung Teduh',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b2734fe255e0ba819e903f5fc4e5",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Aku Tenang - Fourtwnty',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b273c72f1ef7daf938592186bc85",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Dialog Hujan - Senar Senja',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "http://images.genius.com/3d7767eaf6e73a6a738cbcea589c3a88.700x700x1.jpg",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Sebelah Mata - Efek Rumah Kaca',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b273c1528165fd2e76da5331b48c",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Sampai Jadi Debu - Banda Neira',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b2738ee03809d40edb632fadd3c0",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Menuju Senja -Payung Teduh',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-        Container(
-          child: Card(
-            elevation: 10.0,
-            child: Column(children: <Widget>[
-              Image.network(
-                "https://i.scdn.co/image/ab67616d0000b273ef3e3c60762b7ed406af95f9",
-                height: 150.0,
-                width: 200.0,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 1.0),
-              Text(
-                'Aroma Dia - Sisitipsi',
-                style: TextStyle(color: Colors.brown, fontSize: 12, fontWeight: FontWeight.bold),
-              ),
-            ]),
-          ),
-        ),
-      ]),
-    );
-  }
-}
-
-class Playlist extends StatelessWidget {
-  final List myfav = [
-    "Desember - Efek Rumah Kaca",
-    "Aku Tenang - Fourtwnty",
-    "Dialog Hujan - Senar Senja",
-    "Sesuatu di Jogja - Adhitia Sofyan",
-    "Sampai Jadi Debu - Banda Neira"
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-              child: ListTile(
-            title: Text(myfav[index], style: TextStyle(fontSize: 20)),
-            subtitle: Text("Lagu dari " + myfav[index]),
-            leading: Icon(Icons.audiotrack),
-          ));
-        },
-        itemCount: myfav.length,
-      ),
-    );
-  }
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate(this._tabBar);
-
-  final TabBar _tabBar;
-
-  @override
-  double get minExtent => _tabBar.preferredSize.height;
-  @override
-  double get maxExtent => _tabBar.preferredSize.height;
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return new Container(
-      child: _tabBar,
-    );
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return false;
+            ),
+            Container(
+              height: 30,
+              margin: const EdgeInsets.only(top: 10.0),
+              child: Text('Gamis hagia gown Wanita', style: TextStyle(fontSize: 20.0)),
+            ),
+            Container(
+              height: 50,
+              child: Text('Rp 145.000', style: TextStyle(color: Colors.orange, fontSize: 30)),
+            ),
+            Container(
+              height: 30,
+              margin: const EdgeInsets.only(top: 10.0),
+              child: Text('Stok Tersedia', style: TextStyle(fontSize: 20.0)),
+            ),
+          ],
+        ));
   }
 }
