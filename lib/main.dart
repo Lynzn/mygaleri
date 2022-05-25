d9a3c07fcf8947590a92bdb3e48a871c4240faa0
FlutLab.io <>
2022-05-25 05:06:12 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.lightGreen,
+      ),
       home: DetailProduct(),
     );
   }
