7e73e0fee825bc009f7c61035bc1e285c41a7bae
FlutLab.io <>
2022-05-25 04:50:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-class detailProduk extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
