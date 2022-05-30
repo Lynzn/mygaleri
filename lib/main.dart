3d048b29817a624d95fefc96919998a3a2c5ebd7
FlutLab.io <>
2022-05-30 08:57:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -73,42 +73,44 @@ class MyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 300,
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 10),
-            Text('Balance',
-                style: TextStyle(
-                  color: Colors.white,
-                )),
-            SizedBox(height: 30),
-            Text('\$5.867.000',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                )),
-            SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Container(
+            width: 300,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('***8983',
+                SizedBox(height: 10),
+                Text('Balance',
                     style: TextStyle(
                       color: Colors.white,
                     )),
-                Text('10/24',
+                SizedBox(height: 30),
+                Text('\$5.867.000',
                     style: TextStyle(
                       color: Colors.white,
+                      fontSize: 24,
                     )),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('***8983',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                    Text('10/24',
+                        style: TextStyle(
+                          color: Colors.white,
+                        )),
+                  ],
+                )
               ],
-            )
-          ],
-        ));
+            )));
   }
 }
