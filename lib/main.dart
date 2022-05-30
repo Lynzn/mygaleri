206b3543a1a65c9b4a8ae319c5f9c3045d96e6d6
FlutLab.io <>
2022-05-30 10:04:52 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade300,
+      backgroundColor: Colors.white,
       body: SafeArea(
           child: Column(
         children: [
