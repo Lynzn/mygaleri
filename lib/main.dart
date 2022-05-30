0eba32124632046a21a73306454bbcd88d3e75ad
FlutLab.io <>
2022-05-30 07:26:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(FeaturedPlants());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BelajarNavBar(),
-    );
-  }
-}
-
-class BelajarNavBar extends StatefulWidget {
-  @override
-  _BelajarNavBarState createState() => _BelajarNavBarState();
-}
-
-class _BelajarNavBarState extends State<BelajarNavBar> {
-  int _selectedNavbar = 0;
-  var _pages = <Widget>[
-    Icon(Icons.home, size: 250, color: Colors.blueGrey),
-    Icon(Icons.assignment, size: 250, color: Colors.blueGrey),
-    Icon(Icons.person, size: 250, color: Colors.blueGrey),
-    Icon(Icons.mail, size: 250, color: Colors.blueGrey),
-  ];
-
-  void _changeSelectedNavBar(int index) {
-    setState(() {
-      _selectedNavbar = index;
-    });
-  }
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Belajar Bottom Navbar"),
-        backgroundColor: Colors.black,
-        centerTitle: true,
-      ),
-      body: Container(
-        child: Center(child: _pages.elementAt(_selectedNavbar)),
-        color: Colors.black,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: ('Beranda'),
-            backgroundColor: Colors.black,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
+            press: () {},
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.assignment),
-            label: ('Pesanan'),
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: ('Akun'),
-            backgroundColor: Colors.black,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.mail),
-            label: ('Inbox'),
-            backgroundColor: Colors.black,
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
+            press: () {},
           ),
         ],
-        currentIndex: _selectedNavbar,
-        selectedItemColor: Colors.green,
-        unselectedItemColor: Colors.grey,
-        showUnselectedLabels: true,
-        onTap: _changeSelectedNavBar,
       ),
     );
   }
