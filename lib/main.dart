6392abdd903e87c8e19a217a017bb59b2a641fb7
FlutLab.io <>
2022-05-30 11:49:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -203,7 +203,6 @@ class _Bills extends State<Bills> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(height: 10),
               Text(
                 'Upcomming bill',
                 style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
