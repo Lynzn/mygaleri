f086c59b816642399926f88f4611232b0398acc8
FlutLab.io <>
2022-05-30 08:35:48 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,81 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Good morning,John!'),
-        actions: [
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      body: SafeArea(
+          child: Column(
+        children: [
+          //appbar
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: Icon(Icons.search),
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Hello John!',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Icon(Icons.search),
+                ),
+              ],
+            ),
           ),
+
+          SizedBox(height: 25),
+
+          Container(
+              width: 300,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                children: [
+                  Text('Balance'),
+                  Text('\$5.867.000'),
+                  Row(
+                    children: [
+                      Text('***8983'),
+                      Text('10/24'),
+                    ],
+                  )
+                ],
+              ))
         ],
-        backgroundColor: Colors.purple,
-      ), //AppBar
-      body: const Center(
-        child: Text(
-          "Geeksforgeeks",
-          style: TextStyle(fontSize: 24),
-        ), //Text
-      ), //Center
-    ), //Scaffold
-    debugShowCheckedModeBanner: false, //Removing Debug Banner
-  )); //MaterialApp
+      )),
+    );
+  }
 }
