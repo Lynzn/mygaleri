70a36b4e0e44c5eff46c551a6e343b0b8d919184
FlutLab.io <>
2022-05-30 02:38:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,34 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        primarySwatch: Colors.lightGreen,
-      ),
-      home: DetailProduct(),
+      home: BelajarNavBar(),
     );
   }
 }
 
-class DetailProduct extends StatelessWidget {
-  Widget build(BuildContext context) {
+class BelajarNavBar extends StatefulWidget {
+  _BelajarNavBarState createState() => _BelajarNavBarState();
+}
+
+class _BelajarNavBarState extends State<BelajarNavBar> {
+  Widget Build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MAMAMA.RA'),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(8),
-          children: <Widget>[
-            Container(
-              height: 250,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage("https://media2.bulgari.com/f_auto,q_auto/production/dw62a0c52b/images/images/1348757.png"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            Container(
-              height: 30,
-              margin: const EdgeInsets.only(top: 10.0),
-              child: Text('Bulgari Dias', style: TextStyle(fontSize: 20)),
-            ),
-            Container(
-              height: 50,
-              child: Text('Rp 24.500.000', style: TextStyle(color: Colors.orange, fontSize: 30)),
-            ),
-            Container(
-              height: 30,
-              margin: const EdgeInsets.only(top: 10.0),
-              child: Text('Stok Tersedia = 2 pieces', style: TextStyle(fontSize: 20.0)),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: Text("Belajar NavBar"),
+      ),
+      body: Center(child: Text('Home Menu')),
+      bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Beranda')),
+        BottomNavigationBarItem(icon: Icon(Icons.assignment), title: Text('Pesanan')),
+        BottomNavigationBarItem(icon: Icon(Icons.mail), title: Text('Inbox')),
+        BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('Profile')),
+      ]),
+    );
   }
 }
